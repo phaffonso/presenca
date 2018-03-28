d5f14d1207c2d9ddea2507f9918859bcb333644e
phaffonso <pedrohaffonso@gmail.com>
2018-03-28 10:54:46 -0300

    adc contabilizacao de aulas extras (em separado)

--- presenca.py
+++ presenca.py
@@ -10,12 +10,12 @@ def time2f(time):
 def calcPresenca(lista):
     horamin = [17.5, 18.5, 20.25, 22]
     horamax = [18.25, 19.35, 21, 23]
-    horarios = [False, False, False]
+    horarios = [False, False, False, False]
     for horario in range(3):
         for hora in lista:
             if(time2f(hora) <= horamax[horario] and time2f(hora) >= horamin[horario]):
                 horarios[horario] = True
-    return [horarios[0] and horarios[1],horarios[1] and horarios[2]]
+    return [horarios[0] and horarios[1],horarios[1] and horarios[2],horarios[2] and horarios[3]]
     
 
 if(len(sys.argv) != 3):
@@ -46,20 +46,21 @@ with open(in_fn, "r") as fin:
 print 'escrita'    
     
 with open(out_fn, "w") as fout:
-    fout.write('"nome","presencas"\n');
+    fout.write('"nome","presencas","presencas extra"\n');
 #header
     nomes_ord = sorted(list(nomes))
    
     for nome in nomes_ord:
+        soma_extra = 0
         soma = 0
         for dia in dias:
             key = (dia, nome)
-            presenca = [False,False]
+            presenca = [False,False,False]
             if(key in pres):
                 presenca = calcPresenca(pres[key])
-            soma = soma + sum(presenca)
-            print(presenca)
-        fout.write('"' + nome + '",' + str(soma) + '\n')
+            soma = soma + sum(presenca[1:])
+            soma_extra = soma_extra + sum(presenca[0:1])
+        fout.write('"' + nome + '",' + str(soma) + ',' + str(soma_extra) + '\n')
         
         
             
