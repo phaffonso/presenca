1e3a04d627e8783604166ca5c6a91bb7c04927b3
phaffonso <pedrohaffonso@gmail.com>
2018-05-06 11:38:36 -0300

    agora o programa cria um único relatório de saída com base em multiplos arquivos de entrada

--- presenca.py
+++ presenca.py
@@ -1,9 +1,11 @@
-#!/usr/bin/python
+﻿#!/usr/bin/python
 # coding=utf-8
 
 import sys
 import csv
 import os
+import codecs
+import re
 
 def time2f(time):
     h,m,s = time.split(':')
@@ -24,42 +26,68 @@ def calcPresenca(lista):
             if(time2f(hora) <= horaminsaida[horario]):
                 saida = max(horario, saida)
     return [entrada == 0 and saida >= 0, entrada <= 1 and saida >= 1, entrada <= 2 and saida == 2]
-
-def converteArq(in_fn, out_fn):
-
-    nomes = set()
-    dias = set()
-    pres = dict()
-
+    
+def leArq(in_fn, nomes, dias, pres):
     with open(in_fn, "r") as fin:
         next(fin)#pula primeira linha
         for line in fin :
             fields = line.split(";")
             data,hora,evento,codigo,nome,matricula,portal,null = fields
-            nomes.add(nome)
+            nomes.add(normalizaNome(nome))
             dias.add(data)
-            key = (data, nome)
+            key = (data, normalizaNome(nome))
             if(key in pres):
                 pres[key].add(hora)
             else:
                 pres[key] = set([hora])
     
+class Aluno:
+    def __init__(self, row):
+        self.inscricao = row[0];
+        self.nome = row[1].decode('utf-8').upper();
+        self.sala = row[3];
+        
+        
+def carregaAlunos(in_fn):
+    alunos = [];
+    with open(in_fn, "r") as csvfile:
+        reader = csv.reader(csvfile);
+        reader.next(); #descarta cabecalho
+        for row in reader:
+            alunos.append(Aluno(row));
+    return alunos;
+
     
-    with open(out_fn, "w") as fout:
+def converteArq(in_fn, out_fn):
+    nomes = set()
+    dias = set()
+    pres = dict()
+    leArq(in_fn, nomes, dias, pres);
+    escreveArq(out_fn, nomes, dias, pres);
+
+
+def escreveArq(out_fn, nomes, dias, pres, alunos):       
+    with codecs.open(out_fn, "w", encoding='utf8') as fout:
         #escrever cabecalho
         nomes_ord = sorted(list(nomes))
         dias_ord = sorted(list(dias), key = lambda dia: dia.split('/')[::-1])  
-        fout.write('"nome"')
+        fout.write('"insrcicao", "nome","sala"')
         for dia in dias_ord:
             fout.write(',"'+str(dia)+'"')
         fout.write(',"presencas normais","presencas extra"\n');
         
+        perdidos = [];
         for nome in nomes_ord:
             soma_extra = 0
             soma = 0
-            fout.write('"' + nome + '"')
+            if(nome in alunos):
+                aluno = alunos[nome]
+                fout.write(aluno.inscricao+',"' + aluno.nome + '","'+aluno.sala+'"')
+            else:
+                perdidos.append(nome);
+                continue;
             for dia in dias_ord:
-                key = (dia, nome)
+                key = (dia, normalizaNome(nome))
                 presenca = [False,False,False]
                 if(key in pres):
                     presenca = calcPresenca(pres[key])
@@ -69,20 +97,34 @@ def converteArq(in_fn, out_fn):
             fout.write(',' + str(soma) + ',' + str(soma_extra) + '\n')
             
         #escrever totais
+        if(len(perdidos) > 0):
+            print "Alunos nao presentes na listagem:"
+            for p in perdidos:
+                print p
         #fout.write('"Totais/médias"')
         #fout.write('\n')
-            
-# if(len(sys.argv) != 3):
-    # print 'Uso correto: python presenca.py arq_entrada.csv arq_saida.csv'
-    # sys.exit(0)
-# in_fn = sys.argv[1]
-# out_fn = sys.argv[2]            
+
+def normalizaNome(nome):
+    return re.sub("[^a-zA-Z0-9 ]", "", nome)[:23]
+
 
 def main():
+    alunos = carregaAlunos("alunos.csv");
+    dic_alunos = {};
+    for aluno in alunos:
+        dic_alunos[normalizaNome(aluno.nome)] = aluno;
     fn_in = next(os.walk('./in'))[2]
+    nomes = set()
+    dias = set()
+    pres = dict()
     for fn in fn_in:
-        converteArq('in/'+fn, 'out/'+fn)
-        
+        leArq('in/'+fn, nomes, dias, pres)
+    nomes = set([nome.decode('utf-8') for nome in nomes])
+    for nome in [aluno.nome for aluno in alunos]:
+        nomes.add(nome);
+    nomes = set([normalizaNome(nome) for nome in nomes])
+    escreveArq('relatorio_consolidado.csv', nomes, dias, pres, dic_alunos)
+    print u"Relatório criado com sucesso"
 
 if __name__ == "__main__":
     main()
